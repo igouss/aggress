9bd3af9a24455f228c96fb72c948bab4cce2e8d5
Iouri Goussev <i.gouss@gmail.com>
2016-01-03 00:27:39 -0500

    fix price and special

--- crawler/src/main/java/com/naxsoft/parsers/productParser/LeverarmsRawPageParser.java
+++ crawler/src/main/java/com/naxsoft/parsers/productParser/LeverarmsRawPageParser.java
@@ -6,16 +6,12 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
-import java.text.NumberFormat;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,17 +19,13 @@ import java.util.regex.Pattern;
 /**
  * Copyright NAXSoft 2015
  */
-public class LeverarmsRawPageParser  extends AbstractRawPageParser {
+public class LeverarmsRawPageParser extends AbstractRawPageParser {
     private static final Logger logger = LoggerFactory.getLogger(LeverarmsRawPageParser.class);
 
     private static String parsePrice(String price) {
         Matcher matcher = Pattern.compile("\\$((\\d+|,)+\\.\\d+)").matcher(price);
         if (matcher.find()) {
-            try {
-                return NumberFormat.getInstance(Locale.US).parse(matcher.group(1)).toString();
-            } catch (Exception ignored) {
-                return Double.valueOf(matcher.group(1)).toString();
-            }
+            return matcher.group(1);
         } else {
             return price;
         }
@@ -62,7 +54,7 @@ public class LeverarmsRawPageParser  extends AbstractRawPageParser {
         jsonBuilder.field("productImage", document.select(".product-img-box img").attr("abs:src"));
 
         Elements specialPrice = document.select(".special-price .price");
-        if (specialPrice.isEmpty()) {
+        if (!specialPrice.isEmpty()) {
             jsonBuilder.field("regularPrice", parsePrice(document.select(".old-price .price").text()));
             jsonBuilder.field("specialPrice", parsePrice(specialPrice.text()));
         } else {
