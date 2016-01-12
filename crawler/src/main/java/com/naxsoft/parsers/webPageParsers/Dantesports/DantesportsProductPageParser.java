bac63886874eb61df25124545cb61b0843b159b2
Iouri Goussev <i.gouss@gmail.com>
2016-01-12 00:33:27 -0500

    refactor

--- crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Dantesports/DantesportsProductPageParser.java
+++ crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Dantesports/DantesportsProductPageParser.java
@@ -12,45 +12,16 @@ import rx.Observable;
 import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Future;
 
 /**
  * Copyright NAXSoft 2015
  */
 public class DantesportsProductPageParser extends AbstractWebPageParser {
-    private final AsyncFetchClient client;
     private static final Logger logger = LoggerFactory.getLogger(DantesportsProductPageParser.class);
+    private final AsyncFetchClient client;
 
     public DantesportsProductPageParser(AsyncFetchClient client) {
         this.client = client;
-    }
-
-    public Observable<WebPageEntity> parse(WebPageEntity webPage) throws Exception {
-        List<Cookie> cookies = new LinkedList<>();
-
-        Future<List<Cookie>> future = client.get("https://shop.dantesports.com/set_lang.php?lang=EN", cookies, getEngCookiesHandler(), false);
-        cookies.addAll(future.get());
-
-        return Observable.create(subscriber -> {
-            client.get(webPage.getUrl(), cookies, new CompletionHandler<Void>() {
-                @Override
-                public Void onCompleted(com.ning.http.client.Response resp) throws Exception {
-                    if (200 == resp.getStatusCode()) {
-                        WebPageEntity webPageEntity = new WebPageEntity();
-                        webPageEntity.setUrl(webPage.getUrl());
-                        webPageEntity.setContent(compress(resp.getResponseBody()));
-                        webPageEntity.setModificationDate(new Timestamp(System.currentTimeMillis()));
-                        webPageEntity.setParsed(false);
-                        webPageEntity.setStatusCode(resp.getStatusCode());
-                        webPageEntity.setType("productPageRaw");
-                        subscriber.onNext(webPageEntity);
-                        logger.info("productPageRaw={}", webPageEntity.getUrl());
-                    }
-                    subscriber.onCompleted();
-                    return null;
-                }
-            });
-        });
     }
 
     private static CompletionHandler<List<Cookie>> getEngCookiesHandler() {
@@ -58,9 +29,33 @@ public class DantesportsProductPageParser extends AbstractWebPageParser {
             @Override
             public List<Cookie> onCompleted(com.ning.http.client.Response resp) throws Exception {
                 return resp.getCookies();
-
             }
         };
+    }
+
+    public Observable<WebPageEntity> parse(WebPageEntity webPage) throws Exception {
+        return Observable.create(subscriber -> {
+            Observable.from(client.get("https://shop.dantesports.com/set_lang.php?lang=EN", new LinkedList<>(), getEngCookiesHandler(), false)).subscribe(cookies -> {
+                client.get(webPage.getUrl(), cookies, new CompletionHandler<Void>() {
+                    @Override
+                    public Void onCompleted(com.ning.http.client.Response resp) throws Exception {
+                        if (200 == resp.getStatusCode()) {
+                            WebPageEntity webPageEntity = new WebPageEntity();
+                            webPageEntity.setUrl(webPage.getUrl());
+                            webPageEntity.setContent(compress(resp.getResponseBody()));
+                            webPageEntity.setModificationDate(new Timestamp(System.currentTimeMillis()));
+                            webPageEntity.setParsed(false);
+                            webPageEntity.setStatusCode(resp.getStatusCode());
+                            webPageEntity.setType("productPageRaw");
+                            logger.info("productPageRaw={}", webPageEntity.getUrl());
+                            subscriber.onNext(webPageEntity);
+                        }
+                        subscriber.onCompleted();
+                        return null;
+                    }
+                });
+            });
+        });
     }
 
     public boolean canParse(WebPageEntity webPage) {
