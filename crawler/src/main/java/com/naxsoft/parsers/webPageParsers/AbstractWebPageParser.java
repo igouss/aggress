ad5093031aad4a8bbbe36ba18e978e76c1f277b1
Iouri Goussev <i.gouss@gmail.com>
2016-01-13 00:19:52 -0500

    Move cookie handler to base class

--- crawler/src/main/java/com/naxsoft/parsers/webPageParsers/AbstractWebPageParser.java
+++ crawler/src/main/java/com/naxsoft/parsers/webPageParsers/AbstractWebPageParser.java
@@ -1,16 +1,26 @@
 package com.naxsoft.parsers.webPageParsers;
 
+import com.naxsoft.crawler.CompletionHandler;
+import com.ning.http.client.cookie.Cookie;
 import org.apache.commons.io.IOUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
 /**
  * Copyright NAXSoft 2015
  */
 public abstract class AbstractWebPageParser implements WebPageParser {
+    private final static CompletionHandler<List<Cookie>> cookieHandler = new CompletionHandler<List<Cookie>>() {
+        @Override
+        public List<Cookie> onCompleted(com.ning.http.client.Response resp) throws Exception {
+            return resp.getCookies();
+        }
+    };
+
     /**
      * ZIP the string and return Base64 representation
      *
@@ -32,4 +42,7 @@ public abstract class AbstractWebPageParser implements WebPageParser {
 
     }
 
+    protected static CompletionHandler<List<Cookie>> getCookiesHandler() {
+        return cookieHandler;
+    }
 }
