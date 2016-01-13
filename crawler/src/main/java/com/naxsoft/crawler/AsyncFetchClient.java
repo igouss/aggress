81f46b377fc267886372047e1e61c0ebf66d808e
Iouri Goussev <i.gouss@gmail.com>
2016-01-12 23:39:34 -0500

    Exatract HTTP client interface

--- crawler/src/main/java/com/naxsoft/crawler/AsyncFetchClient.java
+++ crawler/src/main/java/com/naxsoft/crawler/AsyncFetchClient.java
@@ -1,100 +1,74 @@
 package com.naxsoft.crawler;
 
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.ListenableFuture;
 import com.ning.http.client.cookie.Cookie;
-import com.ning.http.client.filter.FilterContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.SSLContext;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Copyright NAXSoft 2015
  */
-public class AsyncFetchClient implements AutoCloseable, Cloneable {
-    public static final int REQUEST_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(60);
-    private static final Logger logger = LoggerFactory.getLogger(AsyncFetchClient.class);
-    private final AsyncHttpClient asyncHttpClient;
+public interface AsyncFetchClient {
+    /**
+     * Perform an HTTP GET request
+     * @param url Page address
+     * @param handler Completion handler
+     * @param <R> resource type returned by Completion handler
+     * @return
+     */
+    <R> ListenableFuture<R> get(String url, CompletionHandler<R> handler);
 
-    public AsyncFetchClient(SSLContext sslContext) {
-        AsyncHttpClientConfig asyncHttpClientConfig = new AsyncHttpClientConfig.Builder()
-                .setAcceptAnyCertificate(true)
-                .setSSLContext(sslContext)
-                .setAllowPoolingConnections(false)
-                .setAllowPoolingSslConnections(false)
-                .setMaxRequestRetry(10)
-                .setPooledConnectionIdleTimeout((int) TimeUnit.MILLISECONDS.convert(60, TimeUnit.SECONDS))
-                .setAcceptAnyCertificate(true)
-                .addIOExceptionFilter(ctx -> {
-                    logger.error("ASyncHttpdClient error {} {}", ctx.getRequest().getUrl(), ctx.getIOException().getMessage());
-                    return new FilterContext.FilterContextBuilder(ctx)
-                            .request(ctx.getRequest())
-                            .replayRequest(true)
-                            .build();
-                })
-                .build();
-        asyncHttpClient = new AsyncHttpClient(asyncHttpClientConfig);
-    }
+    /**
+     * Perform an HTTP GET request
+     * @param url Page address
+     * @param cookies Request cookies
+     * @param handler Completion handler
+     * @param <R> result of an asynchronous computation.
+     * @return
+     */
+    <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler);
 
-    public <R> ListenableFuture<R> get(String url, CompletionHandler<R> handler) {
-        return get(url, Collections.<Cookie>emptyList(), handler);
-    }
+    /**
+     * Perform an HTTP GET request
+     * @param url Page address
+     * @param cookies Request cookies
+     * @param handler Completion handler
+     * @param followRedirect Follow HTTP redirects
+     * @param <R> result of an asynchronous computation.
+     * @return
+     */
+    <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler, boolean followRedirect);
 
-    public <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler) {
-        return get(url, cookies, handler, true);
-    }
+    /**
+     * Perform an HTTP POST request
+     * @param url Page address
+     * @param content Content to send in a POST request
+     * @param handler Completion handler
+     * @param <R> result of an asynchronous computation.
+     * @return
+     */
+    <R> ListenableFuture<R> post(String url, String content, CompletionHandler<R> handler);
 
-    public <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler, boolean followRedirect) {
-        logger.trace("Starting async http GET request url = {}", url);
-        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.prepareGet(url);
-        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
-        requestBuilder.setCookies(cookies);
-        requestBuilder.setFollowRedirects(followRedirect);
+    /**
+     * Perform an HTTP POST request
+     * @param url Page address
+     * @param content Content to send in a POST request
+     * @param cookies Request cookies
+     * @param handler Completion handler
+     * @param <R> result of an asynchronous computation.
+     * @return
+     */
+    <R> ListenableFuture<R> post(String url, String content, Collection<Cookie> cookies, CompletionHandler<R> handler);
 
-        ListenableFuture<R> result = requestBuilder.execute(handler);
-
-        return result;
-    }
-
-    public <T> ListenableFuture<T> post(String url, String content, CompletionHandler<T> handler) {
-        return post(url, content, Collections.<Cookie>emptyList(), handler);
-    }
-
-    public <T> ListenableFuture<T> post(String url, String content, Collection<Cookie> cookies, CompletionHandler<T> handler) {
-        logger.trace("Starting async http POST request url = {}", url);
-        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url);
-        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
-        requestBuilder.setCookies(cookies);
-        requestBuilder.setBody(content);
-        requestBuilder.setFollowRedirects(true);
-
-
-        return requestBuilder.execute(handler);
-    }
-
-    public <T> ListenableFuture<T> post(String url, Map<String, String> formParameters, Collection<Cookie> cookies, CompletionHandler<T> handler) {
-        logger.trace("Starting async http POST request url = {}", url);
-        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url);
-        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
-        requestBuilder.setCookies(cookies);
-        requestBuilder.setFollowRedirects(true);
-
-        Set<Map.Entry<String, String>> entries = formParameters.entrySet();
-        for (Map.Entry<String, String> e : entries) {
-            requestBuilder.addFormParam(e.getKey(), e.getValue());
-        }
-
-        return requestBuilder.execute(handler);
-    }
-
-    public void close() {
-        asyncHttpClient.close();
-    }
+    /**
+     * Perform an HTTP POST request
+     * @param url Page address
+     * @param formParameters HTTP Form paramaters
+     * @param cookies Request cookies
+     * @param handler Completion handler
+     * @param <R> result of an asynchronous computation.
+     * @return
+     */
+    <R> ListenableFuture<R> post(String url, Map<String, String> formParameters, Collection<Cookie> cookies, CompletionHandler<R> handler);
 }
---
+++ crawler/src/main/java/com/naxsoft/crawler/AsyncFetchClientImpl.java
@@ -0,0 +1,109 @@
+package com.naxsoft.crawler;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.ListenableFuture;
+import com.ning.http.client.cookie.Cookie;
+import com.ning.http.client.extra.ThrottleRequestFilter;
+import com.ning.http.client.filter.FilterContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.SSLContext;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Copyright NAXSoft 2015
+ */
+public class AsyncFetchClientImpl implements AutoCloseable, Cloneable, AsyncFetchClient {
+    public static final int REQUEST_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(60);
+    private static final Logger logger = LoggerFactory.getLogger(AsyncFetchClientImpl.class);
+    private final AsyncHttpClient asyncHttpClient;
+    private final int maxConnections = 1;
+
+    public AsyncFetchClientImpl(SSLContext sslContext) {
+        AsyncHttpClientConfig asyncHttpClientConfig = new AsyncHttpClientConfig.Builder()
+                .setAcceptAnyCertificate(true)
+                .setSSLContext(sslContext)
+                .setAllowPoolingConnections(false)
+                .setAllowPoolingSslConnections(false)
+                .setMaxRequestRetry(10)
+                .setPooledConnectionIdleTimeout((int) TimeUnit.MILLISECONDS.convert(60, TimeUnit.SECONDS))
+                .setAcceptAnyCertificate(true)
+                .addIOExceptionFilter(ctx -> {
+                    logger.error("ASyncHttpdClient error {} {}", ctx.getRequest().getUrl(), ctx.getIOException().getMessage());
+                    return new FilterContext.FilterContextBuilder(ctx)
+                            .request(ctx.getRequest())
+                            .replayRequest(true)
+                            .build();
+                })
+                .addRequestFilter(new ThrottleRequestFilter(maxConnections))
+                .build();
+        asyncHttpClient = new AsyncHttpClient(asyncHttpClientConfig);
+    }
+
+    @Override
+    public <R> ListenableFuture<R> get(String url, CompletionHandler<R> handler) {
+        return get(url, Collections.<Cookie>emptyList(), handler);
+    }
+
+    @Override
+    public <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler) {
+        return get(url, cookies, handler, true);
+    }
+
+    @Override
+    public <R> ListenableFuture<R> get(String url, Collection<Cookie> cookies, CompletionHandler<R> handler, boolean followRedirect) {
+        logger.trace("Starting async http GET request url = {}", url);
+        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.prepareGet(url);
+        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
+        requestBuilder.setCookies(cookies);
+        requestBuilder.setFollowRedirects(followRedirect);
+
+        ListenableFuture<R> result = requestBuilder.execute(handler);
+
+        return result;
+    }
+
+    @Override
+    public <R> ListenableFuture<R> post(String url, String content, CompletionHandler<R> handler) {
+        return post(url, content, Collections.<Cookie>emptyList(), handler);
+    }
+
+    @Override
+    public <R> ListenableFuture<R> post(String url, String content, Collection<Cookie> cookies, CompletionHandler<R> handler) {
+        logger.trace("Starting async http POST request url = {}", url);
+        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url);
+        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
+        requestBuilder.setCookies(cookies);
+        requestBuilder.setBody(content);
+        requestBuilder.setFollowRedirects(true);
+
+
+        return requestBuilder.execute(handler);
+    }
+
+    @Override
+    public <R> ListenableFuture<R> post(String url, Map<String, String> formParameters, Collection<Cookie> cookies, CompletionHandler<R> handler) {
+        logger.trace("Starting async http POST request url = {}", url);
+        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url);
+        requestBuilder.setRequestTimeout(REQUEST_TIMEOUT);
+        requestBuilder.setCookies(cookies);
+        requestBuilder.setFollowRedirects(true);
+
+        Set<Map.Entry<String, String>> entries = formParameters.entrySet();
+        for (Map.Entry<String, String> e : entries) {
+            requestBuilder.addFormParam(e.getKey(), e.getValue());
+        }
+
+        return requestBuilder.execute(handler);
+    }
+
+    public void close() {
+        asyncHttpClient.close();
+    }
+}
