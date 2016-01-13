1f576911eddbedd8ae7ced07b09041a9244827d3
Iouri Goussev <i.gouss@gmail.com>
2016-01-13 00:42:26 -0500

    Help reflection deal with the fact that com.naxsoft.crawler.AsyncFetchClient is now an interface

--- crawler/src/main/java/com/naxsoft/parsers/webPageParsers/WebPageParserFactory.java
+++ crawler/src/main/java/com/naxsoft/parsers/webPageParsers/WebPageParserFactory.java
@@ -11,6 +11,7 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InvalidClassException;
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
@@ -31,7 +32,17 @@ public class WebPageParserFactory {
             if (!Modifier.isAbstract(clazz.getModifiers())) {
                 try {
                     logger.info("Instantiating {}", clazz.getName());
-                    WebPageParser webPageParser = clazz.getConstructor(client.getClass()).newInstance(client);
+                    Class<?>[] interfaces = client.getClass().getInterfaces();
+                    Class<?> asyncFetchClient = null;
+                    for (Class iface : interfaces) {
+                        if (iface.getCanonicalName().equals("com.naxsoft.crawler.AsyncFetchClient")) {
+                            asyncFetchClient = iface;
+                        }
+                    }
+                    if (null == asyncFetchClient) {
+                        throw new InvalidClassException("Class " + clazz.getCanonicalName() + "should implement com.naxsoft.crawler.AsyncFetchClient");
+                    }
+                    WebPageParser webPageParser = clazz.getConstructor(asyncFetchClient).newInstance(client);
                     this.parsers.add(webPageParser);
                 } catch (Exception e) {
                     logger.error("Failed to instantiate WebPage parser {}", clazz, e);
