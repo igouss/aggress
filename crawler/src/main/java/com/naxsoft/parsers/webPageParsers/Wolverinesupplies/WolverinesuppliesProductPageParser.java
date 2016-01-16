f33ea9766ff3a96dd9a778a2ddf2b626a516a566
Iouri Goussev <i.gouss@gmail.com>
2016-01-15 22:37:18 -0500

    fix logging

--- crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Wolverinesupplies/WolverinesuppliesProductPageParser.java
+++ crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Wolverinesupplies/WolverinesuppliesProductPageParser.java
@@ -27,7 +27,7 @@ public class WolverinesuppliesProductPageParser extends AbstractWebPageParser {
                     if (null != data) {
                         return true;
                     } else {
-                        logger.error("failed to download web page {}" + webPage.getUrl());
+                        logger.error("failed to download web page {}", webPage.getUrl());
                         return false;
                     }
                 }).map(webPageEntity -> {
