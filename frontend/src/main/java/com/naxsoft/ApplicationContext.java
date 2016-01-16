94a16d7a0469fc86d30f95aada31711c04474bce
Iouri Goussev <i.gouss@gmail.com>
2016-01-15 22:38:27 -0500

    initialize value

--- frontend/src/main/java/com/naxsoft/ApplicationContext.java
+++ frontend/src/main/java/com/naxsoft/ApplicationContext.java
@@ -4,7 +4,7 @@ package com.naxsoft;
  * Copyright NAXSoft 2015
  */
 public class ApplicationContext {
-    private boolean invalidateTemplateCache;
+    private boolean invalidateTemplateCache = false;
 
     public void setInvalidateTemplateCache(boolean invalidateTemplateCache) {
         this.invalidateTemplateCache = invalidateTemplateCache;
