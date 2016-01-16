08b175732b548156b08f54b3d62d8ba8c37686a9
Iouri Goussev <i.gouss@gmail.com>
2016-01-15 22:38:49 -0500

    remove verbose

--- frontend/src/main/java/com/naxsoft/handlers/IndexHandler.java
+++ frontend/src/main/java/com/naxsoft/handlers/IndexHandler.java
@@ -15,34 +15,23 @@ import java.util.Locale;
  */
 public class IndexHandler implements HttpHandler {
     public static final String REGULAR_NAME = Paths.get("").toAbsolutePath() + "/basedir/thymeleaf/layout.html";
-    public static final String VERBOSE_NAME = Paths.get("").toAbsolutePath() + "/basedir/thymeleaf/layoutVerbose.html";
     private final ApplicationContext context;
     private final TemplateEngine templateEngine;
-    private final boolean isVerbose;
 
-    public IndexHandler(ApplicationContext context, TemplateEngine templateEngine, boolean isVerbose) {
+    public IndexHandler(ApplicationContext context, TemplateEngine templateEngine) {
         this.context = context;
         this.templateEngine = templateEngine;
-        this.isVerbose = isVerbose;
     }
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         if (context.isInvalidateTemplateCache()) {
-            if (isVerbose) {
-                templateEngine.clearTemplateCacheFor(VERBOSE_NAME);
-            } else {
-                templateEngine.clearTemplateCacheFor(REGULAR_NAME);
-            }
+            templateEngine.clearTemplateCacheFor(REGULAR_NAME);
         }
         HashMap<String, String> variables = new HashMap<>();
         Context context = new Context(Locale.getDefault(), variables);
         String result;
-        if (isVerbose) {
-            result = templateEngine.process(VERBOSE_NAME, context);
-        } else {
-            result = templateEngine.process(REGULAR_NAME, context);
-        }
+        result = templateEngine.process(REGULAR_NAME, context);
         exchange.getResponseSender().send(result);
     }
 }
