49e21ae6db6b131d479b56c61ad82940bc95c8b7
Iouri Goussev <i.gouss@gmail.com>
2016-06-13 12:55:38 -0400

    new class with Rx helper method crashOnError

---
+++ crawler/src/main/java/com/naxsoft/utils/Rx.java
@@ -0,0 +1,26 @@
+package com.naxsoft.utils;
+
+import rx.exceptions.OnErrorNotImplementedException;
+import rx.functions.Action1;
+
+/**
+ * Copyright NAXSoft 2015
+ * <p>
+ * Helper gunctions for RxJava
+ */
+public class Rx {
+    public static Action1<Throwable> crashOnError() {
+        final Throwable checkpoint = new Throwable();
+        return throwable -> {
+            StackTraceElement[] stackTrace = checkpoint.getStackTrace();
+            StackTraceElement element = stackTrace[1]; // First element after `crashOnError()`
+            String msg = String.format("onError() crash from subscribe() in %s.%s(%s:%s)",
+                    element.getClassName(),
+                    element.getMethodName(),
+                    element.getFileName(),
+                    element.getLineNumber());
+
+            throw new OnErrorNotImplementedException(msg, throwable);
+        };
+    }
+}
