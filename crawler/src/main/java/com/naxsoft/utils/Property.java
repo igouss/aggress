9a8c7748766014946585a57c57e4a5aa60025553
Iouri Goussev <i.gouss@gmail.com>
2016-06-16 14:52:46 -0400

    Fix NPE

--- crawler/src/main/java/com/naxsoft/utils/Property.java
+++ crawler/src/main/java/com/naxsoft/utils/Property.java
@@ -3,30 +3,50 @@ package com.naxsoft.utils;
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
+/**
+ * @param <T>
+ */
 public class Property<T> {
     private T value;
     private PublishSubject<T> property;
 
-    public Property() {
+    /**
+     *
+     */
+    private Property() {
         property = PublishSubject.create();
     }
 
+    /**
+     * @param value
+     */
     public Property(T value) {
-        super();
+        this();
         setValue(value);
     }
 
+    /**
+     *
+     * @return
+     */
     public T getValue() {
         return value;
     }
 
+    /**
+     *
+     * @param value
+     */
     public void setValue(T value) {
         this.value = value;
         property.onNext(value);
     }
 
+    /**
+     *
+     * @return
+     */
     public Observable<T> observe() {
         return property.asObservable();
     }
-
 }
