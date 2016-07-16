1d9f71c35a0f9cd66bb0e4dd3da8875fa16b2768
Iouri Goussev <i.gouss@gmail.com>
2016-07-16 01:03:31 -0400

    Initial support for tor

--- crawler/src/main/java/com/naxsoft/crawler/ProxyManager.java
+++ crawler/src/main/java/com/naxsoft/crawler/ProxyManager.java
@@ -1,31 +1,45 @@
 package com.naxsoft.crawler;
 
-import com.naxsoft.utils.AppProperties;
-import com.naxsoft.utils.PropertyNotFoundException;
 import org.asynchttpclient.proxy.ProxyServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class ProxyManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProxyManager.class);
     private int current = 0;
     private ArrayList<ProxyServer> proxyServers = null;
+    private List<String> nonProxyHosts;
 
     ProxyManager() {
-        this.proxyServers = new ArrayList<>();
-        try {
-            String proxyHost = AppProperties.getProperty("proxy.host").getValue();
-            int proxyPort = Integer.parseInt(AppProperties.getProperty("proxy.port").getValue());
-            proxyServers.add(new ProxyServer.Builder(proxyHost, proxyPort).build());
-        } catch (PropertyNotFoundException e) {
-            LOGGER.warn("unable to find proxy");
-        }
+        nonProxyHosts = new ArrayList<>();
+        nonProxyHosts.add("localhost");
+        nonProxyHosts.add("elasticsearch");
+        nonProxyHosts.add("redis");
+
+
+
+        ProxyServer torProxy = new ProxyServer.Builder("tor-proxy", 8118)
+                .setNonProxyHosts(nonProxyHosts)
+                .build();
+
+        proxyServers = new ArrayList<>();
+        proxyServers.add(torProxy);
+
+//        try {
+//            String proxyHost = AppProperties.getProperty("proxy.host").getValue();
+//            int proxyPort = Integer.parseInt(AppProperties.getProperty("proxy.port").getValue());
+//            proxyServers.add(new ProxyServer.Builder(proxyHost, proxyPort).build());
+//        } catch (PropertyNotFoundException e) {
+//            LOGGER.warn("unable to find proxy");
     }
 
     synchronized ProxyServer getProxyServer() {
         ProxyServer result = null;
+        //
+
         if (proxyServers.isEmpty()) {
             LOGGER.debug("no proxy");
         } else {
