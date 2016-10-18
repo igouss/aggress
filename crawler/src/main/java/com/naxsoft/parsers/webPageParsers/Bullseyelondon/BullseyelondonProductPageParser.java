ce3b55c86af710de1627247ff5daf869782b4302
Iouri Goussev <i.gouss@gmail.com>
2016-10-18 11:21:44 -0400

    Add Processing productPage logging

--- crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Bullseyelondon/BullseyelondonProductPageParser.java
+++ crawler/src/main/java/com/naxsoft/parsers/webPageParsers/Bullseyelondon/BullseyelondonProductPageParser.java
@@ -18,6 +18,7 @@ class BullseyelondonProductPageParser extends AbstractWebPageParser {
 
     @Override
     public Observable<WebPageEntity> parse(WebPageEntity webPage) {
+        LOGGER.trace("Processing productPage {}", webPage.getUrl());
         return PageDownloader.download(client, webPage, "productPageRaw")
                 .filter(data -> null != data);
     }
